d6ac4e0bb0e65aeb2c81a75e8b76528f1c8e92fa
rwxd <rwxd@pm.me>
2022-11-06 12:51:02 +0100

    rofi theme collection

--- home-manager/programs/rofi/rofi.nix
+++ home-manager/programs/rofi/rofi.nix
@@ -3,6 +3,15 @@
   home.file.".config/rofi/nord.rasi".source = ./nord.rasi;
   home.file.".config/rofi/catppuccin-frappe.rasi".source = ./catppuccin-frappe.rasi;
 
+  home.file = {
+    ".config/rofi/collection".source = pkgs.fetchFromGitHub {
+      owner = "adi1090x";
+      repo = "rofi";
+      rev = "a372d8420f4b411d8847b9281b3d3f3142849962";
+      sha256 = "sha256-fMSWCkpdaF+AhiywUWj2e/ZWFnTp3H9/9dBEYkLylkY=";
+    };
+  };
+
   programs.rofi = {
     enable = true;
     theme = "catppuccin-frappe";

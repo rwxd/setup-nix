f255d07706568653bcbb4d82a814cd7195fe39c8
rwxd <rwxd@pm.me>
2022-12-10 15:51:41 +0100

    update neovim

--- home-manager/programs/neovim/config/lua/rwxd/misc.lua
+++ home-manager/programs/neovim/config/lua/rwxd/misc.lua
@@ -1,6 +1,10 @@
 local nnoremap = require("rwxd.keymap").nnoremap
+local vnoremap = require("rwxd.keymap").vnoremap
 
 nnoremap("<leader>pv", "<cmd>:Ex<CR>")
 
 -- Markdown preview
 nnoremap("<leader>m", "<cmd>:Glow<CR>")
+
+vnoremap("J", ":m '>+1<CR>gv=gv")
+vnoremap("K", ":m '<-2<CR>gv=gv")

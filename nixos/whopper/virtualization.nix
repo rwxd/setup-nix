d16ce74de5ac92cc8a2a6ef9690830a21f16457d
rwxd <rwxd@pm.me>
2023-03-05 22:40:03 +0100

    fix(whopper): update podman

--- nixos/whopper/virtualization.nix
+++ nixos/whopper/virtualization.nix
@@ -6,9 +6,6 @@
 
       # Create a `docker` alias for podman, to use it as a drop-in replacement
       dockerCompat = false;
-
-      # Required for containers under podman-compose to be able to talk to each other.
-      defaultNetwork.dnsname.enable = true;
     };
 
 	docker = {

ccad52e483220a6ee2ed5fcea08e640c6fe9f966
rwxd <rwxd@pm.me>
2023-04-06 22:29:34 +0200

    fix(nugget): corrected virtualization

--- nixos/nugget/virtualization.nix
+++ nixos/nugget/virtualization.nix
@@ -3,12 +3,8 @@
   virtualisation = {
     podman = {
       enable = true;
-
       # Create a `docker` alias for podman, to use it as a drop-in replacement
       dockerCompat = true;
-
-      # Required for containers under podman-compose to be able to talk to each other.
-      defaultNetwork.dnsname.enable = true;
     };
   };
 

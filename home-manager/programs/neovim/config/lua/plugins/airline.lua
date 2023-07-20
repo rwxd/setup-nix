885f5b20dab8778039b7f0d9d2fd9efeaafda9ce
rwxd <git@rwxd.de>
2023-07-20 16:10:00 +0200

    fix(neovim): deactivated airline

--- home-manager/programs/neovim/config/lua/plugins/airline.lua
+++ home-manager/programs/neovim/config/lua/plugins/airline.lua
@@ -1,10 +1,12 @@
-vim.g.airline_theme='catppuccin'
+-- vim.g.airline_theme='catppuccin'
 
 return {
-  {
-  	"vim-airline/vim-airline",
-  },
-  {
-  	"vim-airline/vim-airline-themes",
-  },
+  -- {
+  -- 	"vim-airline/vim-airline",
+  --   enabled = false,
+  -- },
+  -- {
+  -- 	"vim-airline/vim-airline-themes",
+  --   enabled = false,
+  -- },
 }

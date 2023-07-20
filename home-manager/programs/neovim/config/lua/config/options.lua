87c26873334565c103519aa9cf91cc9bc76b4fdd
rwxd <git@rwxd.de>
2023-07-20 09:46:20 +0200

    fix(neovim): opts

--- home-manager/programs/neovim/config/lua/config/options.lua
+++ home-manager/programs/neovim/config/lua/config/options.lua
@@ -1,3 +1,31 @@
 -- Options are automatically loaded before lazy.nvim startup
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
+vim.opt.smartindent = true
+vim.opt.wrap = false
+vim.opt.mouse = "a"
+vim.opt.number = true
+vim.opt.relativenumber = true
+vim.opt.tabstop = 4
+vim.opt.shiftwidth = 4
+vim.opt.scrolloff = 8
+vim.opt.colorcolumn = "100"
+vim.opt.swapfile = false
+vim.opt.backup = false
+vim.opt.hlsearch = false
+
+-- statusline
+vim.opt.laststatus = 3
+
+-- Having longer updatetime (default is 4000 ms = 4 s) leads to noticeable
+-- delays and poor user experience.
+vim.opt.updatetime = 750
+
+vim.opt.termguicolors = true
+
+-- Give more space for displaying messages.
+vim.opt.cmdheight = 1
+
+vim.g.mapleader = " "
+
+vim.g.black_skip_string_normalization = 1

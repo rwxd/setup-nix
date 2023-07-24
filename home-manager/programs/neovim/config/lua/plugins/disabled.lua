7efe17ccbdfccadfc0b3e2dbedc8b5fb71d469c9
rwxd <git@rwxd.de>
2023-07-24 10:07:36 +0200

    fix(neovim): update

---
+++ home-manager/programs/neovim/config/lua/plugins/disabled.lua
@@ -0,0 +1,7 @@
+return
+{
+  { "akinsho/bufferline.nvim", enabled = false },
+  { "goolord/alpha-nvim", enabled = false },
+  { "echasnovski/mini.indentscope", enabled = false },
+  { "folke/flash.nvim", enabled = false },
+}

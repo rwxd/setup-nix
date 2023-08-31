25e6d7db8202d053fce25861f13665ec93107ccf
rwxd <git@rwxd.de>
2023-08-31 18:11:25 +0200

    fix(whopper): remove garbage collection cronjob

--- nixos/whopper/configuration.nix
+++ nixos/whopper/configuration.nix
@@ -150,7 +150,6 @@
   services.cron = {
 	enable = true;
 	systemCronJobs = [
-	  "0 0 * * * root /run/current-system/sw/bin/nix-collect-garbage -d"
 	  "* * * * * fwrage pacmd set-source-volume alsa_input.usb-Burr-Brown_from_TI_USB_Audio_CODEC-00.analog-stereo-input 0x25000"
 	];
   };

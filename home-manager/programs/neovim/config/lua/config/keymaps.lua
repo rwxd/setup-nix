7cf216753b2aa8026493707cb8ad63b194253b7c
rwxd <git@rwxd.de>
2024-04-01 19:52:33 +0200

    fix: keymap and autocmds

--- home-manager/programs/neovim/config/lua/config/keymaps.lua
+++ home-manager/programs/neovim/config/lua/config/keymaps.lua
@@ -2,28 +2,13 @@
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
 
-local Util = require("lazyvim.util")
-local function map(mode, lhs, rhs, opts)
-	local keys = require("lazy.core.handler").handlers.keys
-	---@cast keys LazyKeysHandler
-	-- do not create the keymap if a lazy keys handler exists
-	if not keys.active[keys.parse({ lhs, mode = mode }).id] then
-		opts = opts or {}
-		opts.silent = opts.silent ~= false
-		if opts.remap and not vim.g.vscode then
-			opts.remap = nil
-		end
-		vim.keymap.set(mode, lhs, rhs, opts)
-	end
-end
-
 -- Movement
-map("v", "J", ":m '>+1<CR>gv=gv", { desc = "Move selection up" })
-map("v", "K", ":m '<-2<CR>gv=gv", { desc = "Move selection down" })
+vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv", { desc = "Move selection up" })
+vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv", { desc = "Move selection down" })
 
 -- File
-map("n", "<leader>pv", "<cmd>:Ex<cr>", { desc = "Show file explorer" })
-map("n", "<leader>x", "<cmd>!chmod +x %<CR>", { desc = "Make file executable" })
+vim.keymap.set("n", "<leader>pv", "<cmd>:Ex<cr>", { desc = "Show file explorer" })
+vim.keymap.set("n", "<leader>x", "<cmd>!chmod +x %<CR>", { desc = "Make file executable" })
 
 -- Go
-map("n", "<leader>ee", "oif err != nil {<CR>}<ESC>Oreturn err<ESC>", { desc = "Add golang err != nil" })
+vim.keymap.set("n", "<leader>ee", "oif err != nil {<CR>}<ESC>Oreturn err<ESC>", { desc = "Add golang err != nil" })

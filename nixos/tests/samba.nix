d106dfef75dd31163829c6e2838b5f4786365c3c
rwxd <git@rwxd.de>
2024-09-20 15:30:35 +0200

    fix: remove samba

--- nixos/tests/samba.nix
+++ nixos/tests/samba.nix
@@ -9,56 +9,6 @@
   networking.firewall.allowedUDPPorts = [
     3702 # wsdd
   ];
-  services.samba.openFirewall = true;
-  services.samba = {
-    enable = true;
-    securityType = "user";
-    extraConfig = ''
-      workgroup = workgroup
-      server string = smbnix
-      netbios name = smbnix
-      security = user
-      #use sendfile = yes
-      #max protocol = smb2
-      # note: localhost is the ipv6 localhost ::1
-      hosts allow = 192.168.122. 192.168.0. 127.0.0.1 localhost
-      hosts deny = 0.0.0.0/0
-      guest account = nobody
-      map to guest = bad user
-    '';
-    shares = {
-      schulung = {
-        path = "/home/fwrage/schulung_share";
-        browseable = "yes";
-        "read only" = "no";
-        "guest ok" = "no";
-        # "create mask" = "0644";
-        # "directory mask" = "0755";
-        # "force user" = "username";
-        # "force group" = "groupname";
-      };
-      public = {
-        path = "/mnt/shares/public";
-        browseable = "yes";
-        "read only" = "no";
-        "guest ok" = "yes";
-        # "create mask" = "0644";
-        # "directory mask" = "0755";
-        # "force user" = "username";
-        # "force group" = "groupname";
-      };
-      private = {
-        path = "/mnt/shares/private";
-        browseable = "yes";
-        "read only" = "no";
-        "guest ok" = "no";
-        # "create mask" = "0644";
-        # "directory mask" = "0755";
-        # "force user" = "username";
-        # "force group" = "groupname";
-      };
-    };
-  };
 
   # This part may be optional for your needs, but I find it makes browsing in Dolphin easier,
   # and it makes connecting from a local Mac possible.
